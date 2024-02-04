06f4aa485dc88621b2469dca1610321dbd77b878
Laurids Wegner <laurids.wegner@nordakademie.de>
2024-02-04 21:31:35 +0100

    started test for train and eval of neuralNetwork

--- implementation/trainNetworkAndEval.py
+++ implementation/trainNetworkAndEval.py
@@ -1,4 +1,5 @@
 from neuralNetwork import neuralNetwork
+import numpy
 
 # number of inputs, hidden, output nodes and learning rate
 input_nodes = 784
@@ -8,3 +9,27 @@ learning_rate = 0.1
 
 # create instance of neural network
 n = neuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
+
+# load the mnist training data CSV file into a list
+training_data_file = open("mnist_dataset/mnist_train.csv", "r")
+training_data_list = training_data_file.readlines()
+training_data_file.close()
+
+
+# train the neural network
+# epochs is the number of times the training data set is used for training
+epochs = 5
+
+for e in range(epochs):
+    print("epoch", e+1, "of", epochs)
+    for record in training_data_list:
+        all_values = record.split(",")
+        # scale and shift the inputs
+        inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
+        # create the target output values (all 0.1, except the desired label which is 0.99)
+        targets = numpy.zeros(output_nodes) + 0.01
+        # all_values[0] is the target label for this record
+        targets[int(all_values[0])] = 0.99
+        n.train(inputs, targets)
+        pass
+    pass
