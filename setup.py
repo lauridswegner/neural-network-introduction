360d64a30f4ce015747110087e94efc8917dc570
Laurids Wegner <laurids.wegner@nordakademie.de>
2024-02-07 22:51:47 +0100

    created a package of the neuralNetwork class

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='neuralNetwork',
+    version='0.1.0',
+    packages=find_packages(),
+    description='create neuralNetwork instances'
+)
